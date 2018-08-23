c6c7f5e61011e6e7b72358d4d12b38ca4ac49f3f
Casey Marshall <casey.s.marshall@gmail.com>
2018-08-23 16:57:14 -0700

    Version 0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.github.csm/voynich "0.1.0-SNAPSHOT"
+(defproject com.github.csm/voynich "0.1.0"
   :description "Multi-party encryption tool."
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
